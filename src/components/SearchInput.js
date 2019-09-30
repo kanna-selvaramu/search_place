e3e8cd8c0cc43c82bd243fbb38024617e06fafbd
Kanna Selvaramu <31565377+kanna-selvaramu@users.noreply.github.com>
2019-09-30 13:28:15 +0530

    Update SearchInput.js

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react';
 import Script from 'react-load-script';
-import { InputGroup, FormControl} from 'react-bootstrap';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import DirectionsIcon from '@material-ui/icons/Directions';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: '2px 4px',
+    display: 'flex',
+    alignItems: 'center',
+    maxWidth: 400,
+    marginTop: 20
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+  },
+  iconButton: {
+    padding: 10,
+  },
+  divider: {
+    height: 28,
+    margin: 4,
+  },
+}));
+
+function CustomizedInput() {
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <IconButton className={classes.iconButton} aria-label="menu">
+        <MenuIcon />
+      </IconButton>
+      <InputBase
+        className={classes.input}
+        id = "autocomplete"
+        placeholder="Search Google Maps"
+        inputProps={{ 'aria-label': 'search google maps' }}
+      />
+      <IconButton className={classes.iconButton} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+      <Divider className={classes.divider} orientation="vertical" />
+      <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+        <DirectionsIcon />
+      </IconButton>
+    </Paper>
+  );
+}
 
 class Search extends Component {
   constructor(props) {
@@ -34,20 +87,7 @@ class Search extends Component {
             onLoad={this.handleScriptLoad}
           />
           <div className="locationWrapper">
-            <InputGroup>
-              <FormControl
-                placeholder="Find your locality"
-                aria-label="Find your locality"
-                id="autocomplete"
-                className = "loc_search"
-              />
-              <div className="icon-wrap">
-                <svg className="svg-inline--fa fa-search fa-w-16" fill="#ccc" aria-hidden="true" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                  <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-                </svg>
-              </div>
-            </InputGroup>
-            <div>{this.state.results}</div>
+            <CustomizedInput />
           </div>
         </div>
       </div>
