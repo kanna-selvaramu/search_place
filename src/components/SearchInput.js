6f6a3b4767866961b66be2e9105ec458377aa86e
Kanna Selvaramu <31565377+kanna-selvaramu@users.noreply.github.com>
2019-10-03 01:00:56 +0530

    Update SearchInput.js

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import Script from 'react-load-script';
-import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
@@ -8,63 +6,23 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import DirectionsIcon from '@material-ui/icons/Directions';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: '2px 4px',
-    display: 'flex',
-    alignItems: 'center',
-    maxWidth: 400,
-    marginTop: 20
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  iconButton: {
-    padding: 10,
-  },
-  divider: {
-    height: 28,
-    margin: 4,
-  },
-}));
-
-function CustomizedInput() {
-  const classes = useStyles();
-
-  return (
-    <Paper className={classes.root}>
-      <IconButton className={classes.iconButton} aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        className={classes.input}
-        id = "autocomplete"
-        placeholder="Search Google Maps"
-        inputProps={{ 'aria-label': 'search google maps' }}
-      />
-      <IconButton className={classes.iconButton} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider className={classes.divider} orientation="vertical" />
-      <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
-    </Paper>
-  );
-}
+import { loadScriptURL } from '../constants/scriptURL';
+import defaultMessages from '../constants/staticText';
 
 class Search extends Component {
+  googleMapRef = React.createRef()
   constructor(props) {
     super(props);
-    this.state = {
-      city: '',
-      query: '' 
-    };
     this.handleScriptLoad = this.handleScriptLoad.bind(this);
   }
 
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.src = loadScriptURL.initScript;
+    script.async = true;
+    script.onload = () => this.handleScriptLoad();
+    document.body.appendChild(script);
+  }
   handleScriptLoad() { 
     var options = {
       types: ['(cities)'],
@@ -76,19 +34,51 @@ class Search extends Component {
       document.getElementById('autocomplete'),
       options,
     ); 
-    this.autocomplete.addListener('place_changed', this.props.triggerStoreMethod);
+    this.autocomplete.addListener('place_changed',this.props.triggerStoreMethod);
+    this.googleMap = this.createGoogleMap();
+    this.marker = this.createMarker();  
   }
+  
+  createGoogleMap = () =>
+    new window.google.maps.Map(this.googleMapRef.current, {
+      zoom: 16,
+      center: {
+        lat: -33.8688,
+        lng: 151.2195,
+      },
+      disableDefaultUI: true,
+  })
+
+  createMarker = () =>
+    new window.google.maps.Marker({
+      position: { lat: -33.8688, lng: 151.2195 },
+      map: this.googleMap,
+  })
+
   render() {
     return (
-      <div class="InpCont">
-        <div className = "InputWrap">
-          <Script
-            url="https://maps.googleapis.com/maps/api/js?key=AIzaSyCoq4_-BeKtYRIs-3FjJL721G1eP5DaU0g&libraries=places"
-            onLoad={this.handleScriptLoad}
-          />
-          <div className="locationWrapper">
-            <CustomizedInput />
+      <div className = "InpMapWrap">
+        <div className="InpCont">
+          <div className = "InputWrap">
+            <div className="locationWrapper">
+              <Paper className="root">
+                <IconButton className="iconButton" aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <InputBase className="input" id = "autocomplete" placeholder= {defaultMessages.searchPlaces} inputProps={{ 'aria-label': defaultMessages.searchPlaces }}/>
+                <IconButton className="iconButton" aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+                <Divider className="divider" orientation="vertical" />
+                <IconButton color="primary" className="iconButton" aria-label="directions">
+                  <DirectionsIcon />
+                </IconButton>
+              </Paper>
+            </div>
           </div>
+        </div>
+        <div className = "Map_Wrapper">
+          <div className = "map_cont" id="google-map" ref={this.googleMapRef}></div>
         </div>
       </div>
     );
