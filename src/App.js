ca00708322a920d2ee85d70a4887dcb81865b856
Kanna Selvaramu <31565377+kanna-selvaramu@users.noreply.github.com>
2019-10-03 00:59:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,52 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import { Provider } from 'react-redux'; 
 import SearchWrap from './components/SearchWrap.js';
-import {RECEIVED_DATA, CLEAR_DATA} from './action'
+import initStore from './store.js';
 
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
-
-const intialState = {
-  recentSearch : [],
-  count : 0
-};
-
-function reducer(state = intialState, action) {
-  switch (action.type){
-    case RECEIVED_DATA:
-      const getTime = new Date();
-      return {
-        ...state,
-        recentSearch: [...state.recentSearch, action.payload],
-        count: state.count + 1
-      }
-    case CLEAR_DATA:
-      return {
-        ...state,
-        recentSearch : [],
-        count: 0
-      }
-    default:
-      return state;
-  }
-}
-
-const store = createStore(
-  reducer,
-  applyMiddleware(thunk)
-);
+let store = initStore();
 
 class App extends Component {
-
   render() {
     return (
-    <div class="searchPageWrap">
-      <Provider store={store}>
-          <SearchWrap />
-      </Provider>
-    </div>
-  )
-   
+      <div className ="searchPageWrap">
+        <Provider store={store}>
+            <SearchWrap />
+        </Provider>
+      </div>
+    )
   }
 }
 
