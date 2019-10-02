ae9a2085e9a19a58dac319feb77a52a372e53d72
Kanna Selvaramu <31565377+kanna-selvaramu@users.noreply.github.com>
2019-10-03 01:01:25 +0530

    Update SearchWrap.js

--- src/components/SearchWrap.js
+++ src/components/SearchWrap.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {updateHistory,clearHistory} from '../action'
+import {updateHistory, clearHistory } from '../actions/searchActions'
 import Search from '../components/SearchInput';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import SearchResults from '../components/searchResults';
 
-const storeResults = []
 
 class SearchWrap extends Component { 
   constructor(props) 
@@ -15,62 +11,33 @@ class SearchWrap extends Component {
     super(props);
   }
   render() {
-  return (
+    return (
       <div className = "searchCont">
-          <Search triggerStoreMethod={this.props.dispatchSearchResults}/>
-          <div class = "SearchResultsWrap">
-            <div class = "resultsCont">
-              <div class = "resultsTitle">
-                You recently searched for
-              </div>
-              <List className = "RecentSearchesWrapper">
-                {
-                  this.props.renderResults.map(function(name,index){
-                    return (
-                      <ListItem className = "listElement">
-                        <ListItemText primary={name}/>
-                      </ListItem>
-                    )
-                  })
-                }
-              </List>
-              <div className = "resultsWrapper">
-                <div className = "resultsCountCont">
-                  <span className = "resultsCount">
-                    {this.props.count}
-                  </span>
-                  <span className = "resultsText"> results </span>
-                </div>
-                <div className = "resetCont" onClick = {this.props.dispatchClearResults}>
-                  RESET
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-            
+        <Search triggerStoreMethod={this.props.dispatchSearchResults}/>
+        <SearchResults />
+      </div>        
     )
-    }
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-      renderResults: state.recentSearch,
-      count: state.count
-    }
+  return {
+    renderResults: state.recentSearch,
+    count: state.count
   }
+}
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        dispatchSearchResults: () => {
-          let location = document.getElementById('autocomplete');
-          dispatch(updateHistory(location.value));        
-          document.getElementById('autocomplete').value = '';
-        },
-        dispatchClearResults: () => {
-          dispatch(clearHistory());
-        }
+  return {
+    dispatchSearchResults: () => {
+      let location = document.getElementById('autocomplete');
+      dispatch(updateHistory(location.value));
+      document.getElementById('autocomplete').value = '';
+    },
+    dispatchClearResults: () => {
+      dispatch(clearHistory());
     }
+  }
 }
 
 export default connect(mapStateToProps,mapDispatchToProps)(SearchWrap)
