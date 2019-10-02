72797d31bddb46bd11bc7547adf9a28d7724b9eb
Kanna Selvaramu <31565377+kanna-selvaramu@users.noreply.github.com>
2019-10-03 01:08:22 +0530

    Add files via upload

---
+++ src/constants/map.js
@@ -0,0 +1,13 @@
+const googleMapApiKey = 'AIzaSyDejMEw7iAaAFt7QvmHDhiY1NpZK7R-MRw&';
+
+const MAP_JSON = {
+  key: googleMapApiKey,
+  api: 'https://maps.googleapis.com/maps/api',
+  zoom: 14,
+  defaultLocation: {
+    lat: 35.6967329,
+    lng: 51.2097323,
+  },
+};
+
+export default MAP_JSON;
